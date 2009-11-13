e7670e33fb33d3708ab57b3054d29b7b8e438a7a
Chris Lockfort <devnull@csh.rit.edu>
2009-11-13 13:40:10 -0500

    Added more helper functions (gets,putch,etc) and more human-friendly strings.

--- clockyboot.s
+++ clockyboot.s
@@ -8,13 +8,16 @@ start:
 	println dashes
 	println boot_msg
 	println dashes
-	
-.loopme	jmp short .loopme
+	println endl
+	print boot_prompt
+	jmp gets
+.loopme jmp short .loopme
 
 ;data
-dashes    db 45,45,45,45,45,45,45,45,45,45,45,45,45,45,45,45,45,45,45,45,45,0
-boot_msg  db 124,' ClockBoot Started ',124,0
+dashes    db ' -------------------',0
+boot_msg  db '| ClockBoot Started |',0
 endl	db 13,10,0
+boot_prompt db 'What would you like to boot? ',0
 
 reboot:
 	db 0x0ea
@@ -30,6 +33,30 @@ puts:
 	int 0x10
 	jmp puts
 gtfo:
+	ret
+
+gets:
+	mov ah, 0
+	int 0x16 ;getch
+
+	cmp al, 0x08 ;backspace
+	je .back
+	cmp al, 0x0D ;enter
+	je .gtfo
+	
+	mov ah, 0x0e
+	mov bx, 7
+	int 0x10
+	jmp gets	
+.gtfo:
+	ret
+.back:
+	ret
+
+putch:
+	mov ah, 0x0e
+	mov bx, 7
+	int 0x10
 	ret
 
 
