031114d0f3b0fa80ebd4c4b1b0b9702b489d8f01
Chris Lockfort <devnull@csh.rit.edu>
2009-12-04 23:58:59 -0500

    Fixed puts,putch,everything. It even compiles now. Huzzah!

--- vid_colortext.c
+++ vid_colortext.c
@@ -1,19 +1,21 @@
 #define terminal_color 0x07
 #define terminal_width 80
 #define terminal_height 25
-#define video_memory_start 0xb8000;
+#define video_memory_start 0xb8000
 char cursor_x=0;
 char cursor_y=0;
-
+void scroll(){
+}
 void k_clr(){
-	for(unsigned char *vid_mem=video_memory_start; vid_mem < (unsigned char) 0xb8fa0; vid_mem+=2){
+	for(unsigned char *vid_mem=(unsigned char *) video_memory_start;
+		vid_mem < (unsigned char *) 0xb8fa0; vid_mem+=2){
 		*vid_mem=' ';  //set char to space
 		*(vid_mem+1)= terminal_color; //set attribute to default
 	}
 }
 
-void k_putch(char character, char attr){
-	unsigned char *vid_mem=video_memory_start+2*(cursor_x+terminal_width*cursor_y);
+void k_putchar(char character, char attr){
+	unsigned char *vid_mem=(unsigned char *) video_memory_start+2*(cursor_x+terminal_width*cursor_y);
 
 	if(character==13){//CR
 		cursor_x=0;
@@ -48,16 +50,12 @@ void k_putch(char character, char attr){
 
 /* C needs default arguements :-( */
 void k_putch(char c){
-	k_putch(c, terminal_color);
-}
-
-void scroll(){
-	
+	k_putchar(c, terminal_color);
 }
 
 void k_puts(char *string){
-	while(*string != 0)
-		putch(*string);
+	while(*string != 0){
+		k_putch(*string);
 		++string;
 	}
 }
