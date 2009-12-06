c1bb25cc37194d4e2f74bf56f770b2e1166a2c2e
Chris Lockfort <devnull@csh.rit.edu>
2009-12-05 19:10:52 -0500

    CR/LF functionality added

--- kernel.c
+++ kernel.c
@@ -1,5 +1,7 @@
 extern void k_puts(char *);
 extern void k_clr();
+extern void k_putch(char);
+
 int main();
 
 void _start(void)
@@ -8,6 +10,14 @@ void _start(void)
 }
 int main(void){
 	k_clr();
-	k_puts("Video Mode Color Text Driver Online\n");
+	k_puts("Video Mode Color Text Driver Online\r\n");
+	for(int i=0; i<13; i++){
+	k_puts("Writing text to see if scrolling works!\r\n");
+	k_puts("........\r\n");
+	}
+	k_clr();
+	k_puts("Did that clear operation work properly?\r\n");
+
 	while(1){}
 }
+ 
