61788dab327e86b43ba36d2e9b79b9956d1a1d91
Chris Lockfort <devnull@csh.rit.edu>
2010-09-05 05:44:39 -0400

    textmode serial pretty much done

--- v3/src/video/textmode/serial/serial.c
+++ v3/src/video/textmode/serial/serial.c
@@ -23,68 +23,6 @@
 
 #include "serial.h"
 
-#if 0
-// Very simple character echo test
-int main(void)
-{
-	CPU_PRESCALE(0);
-	usb_init();
-	while (1) {
-		int n = usb_serial_getchar();
-		if (n >= 0) usb_serial_putchar(n);
-	}
-}
-
-#else
-
-// Basic command interpreter for controlling port pins
-int main(void)
-{
-	char buf[32];
-	uint8_t n;
-
-	// set for 16 MHz clock, and turn on the LED
-	CPU_PRESCALE(0);
-	LED_CONFIG;
-	LED_ON;
-
-	// initialize the USB, and then wait for the host
-	// to set configuration.  If the Teensy is powered
-	// without a PC connected to the USB port, this 
-	// will wait forever.
-	usb_init();
-	while (!usb_configured()) /* wait */ ;
-	_delay_ms(1000);
-
-	while (1) {
-		block_on_host();
-		
-
-		// discard anything that was received prior.  Sometimes the
-		// operating system or other software will send a modem
-		// "AT command", which can still be buffered.
-		usb_serial_flush_input();
-
-		// print a nice welcome message
-		send_str(PSTR("\r\nTeensy USB Serial Example, "
-			"Simple Pin Control Shell\r\n\r\n"
-			"Example Commands\r\n"
-			"  B0?   Read Port B, pin 0\r\n"
-			"  C2=0  Write Port C, pin 1 LOW\r\n"
-			"  D6=1  Write Port D, pin 6 HIGH  (D6 is LED pin)\r\n\r\n"));
-
-		// and then listen for commands and process them
-		while (1) {
-			send_str(PSTR("> "));
-			n = recv_str(buf, sizeof(buf));
-			if (n == 255) break;
-			send_str(PSTR("\r\n"));
-			parse_and_execute_command(buf, n);
-		}
-	}
-}
-#endif
-
 /* Intialize USB, wait for host. We will _BLOCK_ (!!!) at usb_configured(), 
  * if there was no connected USB host. We will block at host_is_ready()
  * if the USB host isn't ready to recieve (aka doesn't have a terminal emulator running) 
@@ -94,6 +32,7 @@ void initialize(){
 	while (!usb_configured()) /* wait */ ;
 	_delay_ms(1000);
 	block_on_host();
+	usb_serial_flush_input();
 }
 
 uint8_t host_is_ready(){
@@ -148,73 +87,6 @@ uint8_t recv_str(char *buf, uint8_t size)
 		}
 	}
 	return count;
-}
-
-// parse a user command and execute it, or print an error message
-//
-void parse_and_execute_command(const char *buf, uint8_t num)
-{
-	uint8_t port, pin, val;
-
-	if (num < 3) {
-		send_str(PSTR("unrecognized format, 3 chars min req'd\r\n"));
-		return;
-	}
-	// first character is the port letter
-	if (buf[0] >= 'A' && buf[0] <= 'F') {
-		port = buf[0] - 'A';
-	} else if (buf[0] >= 'a' && buf[0] <= 'f') {
-		port = buf[0] - 'a';
-	} else {
-		send_str(PSTR("Unknown port \""));
-		usb_serial_putchar(buf[0]);
-		send_str(PSTR("\", must be A - F\r\n"));
-		return;
-	}
-	// second character is the pin number
-	if (buf[1] >= '0' && buf[1] <= '7') {
-		pin = buf[1] - '0';
-	} else {
-		send_str(PSTR("Unknown pin \""));
-		usb_serial_putchar(buf[0]);
-		send_str(PSTR("\", must be 0 to 7\r\n"));
-		return;
-	}
-	// if the third character is a question mark, read the pin
-	if (buf[2] == '?') {
-		// make the pin an input
-		*(uint8_t *)(0x21 + port * 3) &= ~(1 << pin);
-		// read the pin
-		val = *(uint8_t *)(0x20 + port * 3) & (1 << pin);
-		usb_serial_putchar(val ? '1' : '0');
-		send_str(PSTR("\r\n"));
-		return;
-	}
-	// if the third character is an equals sign, write the pin
-	if (num >= 4 && buf[2] == '=') {
-		if (buf[3] == '0') {
-			// make the pin an output
-			*(uint8_t *)(0x21 + port * 3) |= (1 << pin);
-			// drive it low
-			*(uint8_t *)(0x22 + port * 3) &= ~(1 << pin);
-			return;
-		} else if (buf[3] == '1') {
-			// make the pin an output
-			*(uint8_t *)(0x21 + port * 3) |= (1 << pin);
-			// drive it high
-			*(uint8_t *)(0x22 + port * 3) |= (1 << pin);
-			return;
-		} else {
-			send_str(PSTR("Unknown value \""));
-			usb_serial_putchar(buf[3]);
-			send_str(PSTR("\", must be 0 or 1\r\n"));
-			return;
-		}
-	}
-	// otherwise, error message
-	send_str(PSTR("Unknown command \""));
-	usb_serial_putchar(buf[0]);
-	send_str(PSTR("\", must be ? or =\r\n"));
 }
 
 
