d77299f3d9fdb7569e9dd6f5fbe3fc5b496188aa
Chris Lockfort <devnull@csh.rit.edu>
2010-09-05 05:36:09 -0400

    working on textmode USB-Serial driver

---
+++ v3/src/video/textmode/serial/serial.h
@@ -0,0 +1,23 @@
+#ifndef serial_h
+#define serial_h
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <stdint.h>
+#include <util/delay.h>
+#include "usb_serial.h"
+
+#define CPU_PRESCALE(n) (CLKPR = 0x80, CLKPR = (n))
+
+#define LED_CONFIG	(DDRD |= (1<<6))
+#define LED_ON		(PORTD &= ~(1<<6))
+#define LED_OFF		(PORTD |= (1<<6))
+#define CPU_PRESCALE(n) (CLKPR = 0x80, CLKPR = (n))
+
+void initialize(void);
+uint8_t host_is_ready(void);
+void block_on_host(void);
+void send_str(const char *s);
+uint8_t recv_str(char *buf, uint8_t size);
+void parse_and_execute_command(const char *buf, uint8_t num);
+
+#endif
